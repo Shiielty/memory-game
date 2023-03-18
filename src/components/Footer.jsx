9a8a081183e0b9cd32d19c3c9d29d220e54020a8
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-03-18 14:22:37 +0700

    Create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        Created by <a href="https://github.com/Shiielty">Shiielty</a> &#169;
+        2023 | <a href="https://github.com/Shiielty/memory-game">Source Code</a>{' '}
+        | Images from{' '}
+        <a href="https://heaven-burns-red.com/fankit/">
+          Heaven Burns Red Fankit
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
